e69380095bb6a01f01e51280186fcb36b113c0c6
Ghanshyam Thakkar <shyamthakkar001@gmail.com>
2024-05-27 05:07:13 +0530

    t/: migrate helper/test-oidtree.c to unit-tests/t-oidtree.c
    helper/test-oidtree.c along with t0069-oidtree.sh test the oidtree.h
    library, which is a wrapper around crit-bit tree. It is useful in
    string related operations (e.g. find suffixes of a string in the
    tree).
    
    Migrate them to the unit testing framework for better debugging and
    runtime performance.
    
    To achieve this, introduce a new library called 'lib-oid.h'
    exclusively for the unit tests to use. It currently mainly includes
    utility to generate object_id from an arbitrary hex string such as
    'abc'. This will also be helpful when we port other unit tests such
    as test-oid-array.c
    
    Mentored-by: Christian Couder <chriscool@tuxfamily.org>
    Mentored-by: Kaartic Sivaraam <kaartic.sivaraam@gmail.com>
    Signed-off-by: Ghanshyam Thakkar <shyamthakkar001@gmail.com>

---
+++ t/unit-tests/lib-oid.c
@@ -0,0 +1,47 @@
+#include "test-lib.h"
+#include "lib-oid.h"
+#include "strbuf.h"
+#include "hex.h"
+
+static int init_hash_algo(void)
+{
+	static int algo = -1;
+
+	if (algo < 0) {
+		const char *algo_name = getenv("GIT_TEST_DEFAULT_HASH");
+		algo = algo_name ? hash_algo_by_name(algo_name) : GIT_HASH_SHA1;
+
+		if (!check(algo != GIT_HASH_UNKNOWN))
+			test_msg("BUG: invalid GIT_TEST_DEFAULT_HASH value ('%s')", algo_name);
+	}
+	return algo;
+}
+
+static int get_oid_arbitrary_hex_algop(const char *hex, size_t sz,
+				       int hash_algo, struct object_id *oid)
+{
+	int ret;
+	struct strbuf buf = STRBUF_INIT;
+
+	if (!check(sz < hash_algos[hash_algo].hexsz)) {
+		test_msg("BUG: hex string bigger than maximum allowed");
+		return -1;
+	}
+
+	strbuf_add(&buf, hex, sz);
+	strbuf_addchars(&buf, '0', hash_algos[hash_algo].hexsz - sz);
+
+	ret = get_oid_hex_algop(buf.buf, oid, &hash_algos[hash_algo]);
+	strbuf_release(&buf);
+
+	return ret;
+}
+
+int get_oid_arbitrary_hex(const char *hex, struct object_id *oid)
+{
+	int hash_algo = init_hash_algo();
+
+	if (!check_int(hash_algo, !=, GIT_HASH_UNKNOWN))
+		return -1;
+	return get_oid_arbitrary_hex_algop(hex, strlen(hex), hash_algo, oid);
+}
---
+++ t/unit-tests/t-oidtree.c
@@ -0,0 +1,88 @@
+#include "test-lib.h"
+#include "lib-oid.h"
+#include "oidtree.h"
+#include "hash.h"
+#include "hex.h"
+
+#define FILL_TREE(tree, ...)                                       \
+	do {                                                       \
+		const char *hexes[] = { __VA_ARGS__ };             \
+		if (fill_tree_loc(tree, hexes, ARRAY_SIZE(hexes))) \
+			return;                                    \
+	} while (0)
+static int fill_tree_loc(struct oidtree *ot, const char *hexes[], int n)
+{
+	for (size_t i = 0; i < n; i++) {
+		struct object_id oid;
+		if (!check_int(get_oid_arbitrary_hex(hexes[i], &oid), ==, 0))
+			return -1;
+		oidtree_insert(ot, &oid);
+	}
+	return 0;
+}
+
+static void check_contains(struct oidtree *ot, const char *hex, int expected)
+{
+	struct object_id oid;
+
+	if (!check_int(get_oid_arbitrary_hex(hex, &oid), ==, 0))
+		return;
+	if (!check_int(oidtree_contains(ot, &oid), ==, expected))
+		test_msg("oid: %s", oid_to_hex(&oid));
+}
+
+static enum cb_next check_each_cb(const struct object_id *oid, void *data)
+{
+	const char *hex = data;
+	struct object_id expected;
+
+	if (!check_int(get_oid_arbitrary_hex(hex, &expected), ==, 0))
+		return CB_CONTINUE;
+	if (!check(oideq(oid, &expected)))
+		test_msg("expected: %s\n       got: %s",
+			 hash_to_hex(expected.hash), hash_to_hex(oid->hash));
+	return CB_CONTINUE;
+}
+
+static void check_each(struct oidtree *ot, char *hex, char *expected)
+{
+	struct object_id oid;
+	if (!check_int(get_oid_arbitrary_hex(hex, &oid), ==, 0))
+		return;
+	oidtree_each(ot, &oid, 40, check_each_cb, expected);
+}
+
+static void setup(void (*f)(struct oidtree *ot))
+{
+	struct oidtree ot;
+	oidtree_init(&ot);
+	f(&ot);
+	oidtree_clear(&ot);
+}
+
+static void t_insert(struct oidtree *ot)
+{
+	FILL_TREE(ot, "444", "1", "2", "3", "4", "5", "a", "b", "c", "d", "e");
+	check_contains(ot, "44", 0);
+	check_contains(ot, "441", 0);
+	check_contains(ot, "440", 0);
+	check_contains(ot, "444", 1);
+	check_contains(ot, "4440", 1);
+	check_contains(ot, "4444", 0);
+}
+
+static void t_each(struct oidtree *ot)
+{
+	FILL_TREE(ot, "f", "9", "8", "123", "321", "a", "b", "c", "d", "e");
+	check_each(ot, "12300", "123");
+	check_each(ot, "3211", ""); /* should not reach callback */
+	check_each(ot, "3210", "321");
+	check_each(ot, "32100", "321");
+}
+
+int cmd_main(int argc UNUSED, const char **argv UNUSED)
+{
+	TEST(setup(t_insert), "oidtree insert and contains");
+	TEST(setup(t_each), "oidtree each");
+	return test_done();
+}
