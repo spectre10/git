c4b7604cc417e9b5466a2ca4cd02126b87de029d
Ghanshyam Thakkar <shyamthakkar001@gmail.com>
2024-05-27 05:07:13 +0530

    t/: migrate helper/test-oidtree.c to unit-tests/t-oidtree.c
    helper/test-oidtree.c along with t0069-oidtree.sh test the oidtree.h
    library, which is a wrapper around crit-bit tree. It is useful in
    string related operations (e.g. find suffixes of a string in the
    tree).
    
    Migrate them to the unit testing framework for better debugging and
    runtime performance.
    
    To achieve this, introduce a new library called 'lib-oid.h'
    exclusively for the unit tests to use. It currently mainly includes
    utility to generate object_id from an arbitrary hex string such as
    'abc'. This will also be helpful when we port other unit tests such
    as test-oid-array.c
    
    Mentored-by: Christian Couder <chriscool@tuxfamily.org>
    Mentored-by: Kaartic Sivaraam <kaartic.sivaraam@gmail.com>
    Signed-off-by: Ghanshyam Thakkar <shyamthakkar001@gmail.com>

---
+++ t/unit-tests/lib-oid.c
@@ -0,0 +1,46 @@
+#include "test-lib.h"
+#include "lib-oid.h"
+#include "strbuf.h"
+#include "hex.h"
+
+static int init_hash_algo(void)
+{
+	static int algo = -1;
+
+	if (algo < 0) {
+		const char *algo_name = getenv("GIT_TEST_DEFAULT_HASH");
+		algo = algo_name ? hash_algo_by_name(algo_name) : GIT_HASH_SHA1;
+
+		if (!check(algo != GIT_HASH_UNKNOWN)) {
+			test_msg("BUG: invalid GIT_TEST_DEFAULT_HASH value");
+			return -1;
+		}
+	}
+	return algo;
+}
+
+static int get_oid_arbitrary_hex_algop(const char *hex, size_t sz,
+				       int hash_algo, struct object_id *oid)
+{
+	int ret;
+	struct strbuf buf = STRBUF_INIT;
+
+	if (!check(sz < hash_algos[hash_algo].hexsz)) {
+		test_msg("BUG: hex string bigger than maximum allowed");
+		return -1;
+	}
+
+	strbuf_add(&buf, hex, sz);
+	strbuf_addchars(&buf, '0', hash_algos[hash_algo].hexsz - sz);
+
+	ret = get_oid_hex_algop(buf.buf, oid, &hash_algos[hash_algo]);
+	strbuf_release(&buf);
+
+	return ret;
+}
+
+int get_oid_arbitrary_hex(const char *hex, struct object_id *oid)
+{
+	int hash_algo = init_hash_algo();
+	return get_oid_arbitrary_hex_algop(hex, strlen(hex), hash_algo, oid);
+}
